e61da630c0abe88e4a0471d38b91cd9aa1a7cb2b
Prima Wirawan <primailkom@gmail.com>
2021-12-07 15:20:51 +0700

    add the banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 // type "rfc", this works using ES7 react extensions
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
